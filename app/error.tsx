0c3d5611f3ba3fe570e9a2a7b15d34723ef91862
Mahmoud Alshrkawy <mahmoud.alshrkawy@gmail.com>
2026-05-17 20:45:57 +0300

    fix(error): reorder import statements for consistency

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 
 export default function Error({
   error,
